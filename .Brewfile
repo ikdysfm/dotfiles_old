900e411c99cd4a4c25e14b4b7d55ba6c29080359
IKEDA, Yoshifumi <ikd.ysfm@gmail.com>
2014-12-14 01:42:38 +0900

    Created Brewfile

---
+++ .Brewfile
@@ -0,0 +1,42 @@
+# Homebrewを最新版にアップデート
+update
+
+# formulaを更新
+upgrade
+
+# バージョン違いを個別のformulaとして提供する
+tap homebrew/versions
+
+# cask
+tap phinze/homebrew-cask
+
+install zsh
+install git
+install tig
+install tmux
+install wget
+install curl
+install openssl
+
+install brew-cask
+cask install alfred
+cask install karabiner
+cask install bettertouchtool
+cask install xtrafinder
+cask install apptrap
+cask install the-unarchiver
+cask install iterm2
+cask install macvim
+cask install google-chrome
+cask install lastpass
+cask install vlc
+cask install mplayerx
+cask install handbrake
+cask install scansnap-manager
+cask install dropbox
+
+# 不要なファイルを削除
+cleanup
+
+# alfredの検索対象に入れる
+cask alfred link
---
+++ .brew2sh.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import sys
+import os
+
+def print_shellscript_for_brew_bundle(brewfilename):
+    with open(brewfilename) as f:
+        print("#!/bin/sh")
+        for line in f:
+            if line[0] == "#" or len(line) == 1:
+                print(line.rstrip())
+            else:
+                print("brew %s || true" % line.rstrip())
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("brewfile")
+    args = parser.parse_args()
+    if not os.path.isfile(args.brewfile):
+        print("%s is not a file" % args.brewfile)
+        sys.exit(1)
+
+    print_shellscript_for_brew_bundle(args.brewfile)
+
+if __name__ == '__main__':
+    main()
